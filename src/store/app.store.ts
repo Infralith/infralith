216bcc2b4be166c4bcc1384a973dff14441becca
muozez <muhammetylmaz128@gmail.com>
2026-02-21 21:06:36 +0300

    chore: install zustand with sample store

---
+++ src/store/app.store.ts
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+interface AppState {
+  sidebarOpen: boolean;
+  toggleSidebar: () => void;
+  setSidebarOpen: (open: boolean) => void;
+}
+
+export const useAppStore = create<AppState>()(
+  devtools(
+    (set) => ({
+      sidebarOpen: true,
+      toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
+      setSidebarOpen: (open) => set({ sidebarOpen: open }),
+    }),
+    { name: 'app-store' },
+  ),
+);
